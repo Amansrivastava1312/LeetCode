a83c3a575b4f9be43731d30c8a8e3b53d1e0624b
Srivastava Aman S <91677006+Amansrivastava1312@users.noreply.github.com>
2023-09-09 01:28:18 +0530

    Time: 1 ms (99.97%), Space: 44.7 MB (37.38%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        
+        
+        int count=0;
+        
+        for(int i =0;i<nums.length;i++){
+            count=count^nums[i];
+               
+        }
+        return count;
+    }
+}
