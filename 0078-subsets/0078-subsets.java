79de69740954d3574158c77bc9fc967375db1cc6
Srivastava Aman S <91677006+Amansrivastava1312@users.noreply.github.com>
2024-01-15 23:28:06 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (33.73%) - LeetHub

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,21 @@
+class Solution {
+ public void sub (int idx , List<List<Integer>> res,  List<Integer> curr , int [] nums){
+        res.add(new ArrayList(curr));
+        
+        for(int i = idx ; i<nums.length;i++){
+            curr.add(nums[i]);
+            sub(i+1,res,curr,nums);
+            curr.remove(curr.size()-1);
+        }
+    }
+    
+    
+    public List<List<Integer>> subsets(int[] nums) {
+        
+        List<List<Integer>> res = new ArrayList<>();
+        
+        
+        sub(0,res,new ArrayList(),nums);
+        return res;
+    }
+}
