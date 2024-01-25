41f27cb2067ed6fbc1a1b1fd5a2c36a47cec2014
Srivastava Aman S <91677006+Amansrivastava1312@users.noreply.github.com>
2024-01-26 00:41:25 +0530

    Time: 8 ms (57.72%), Space: 57 MB (33.98%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,38 +1,37 @@
 class Solution {
     
-    void part(int index , String s ,List<List<String>> res ,List<String> ans )
-    {
-        
+    void helper(int index , String s,List<List<String>> res ,List<String> ans){
         if(index==s.length()){
             res.add(new ArrayList<>(ans));
             return;
         }
         
-        for(int i=index;i<s.length();++i){
-            if(isPalindrome(s,index , i)){
+        for(int i=index;i<s.length();i++){
+            
+            if(isPalindrome(s,index,i)){
                 ans.add(s.substring(index,i+1));
-                part(i+1,s,res,ans);
-                ans.remove(ans.size()-1);
+                helper(i+1,s,res,ans);
+                    ans.remove(ans.size()-1);
             }
         }
     }
     
-    boolean isPalindrome (String s , int start , int end){
+    boolean isPalindrome(String s,int start,int end){
         while(start<=end){
-                if(s.charAt(start++)!=s.charAt(end--)){
-                    return false;
-                    
-                }
-            // start++;
-            // end--;
+            
+            if(s.charAt(start)!=s.charAt(end)) return false;
+            
+             start++;
+        end--;
         }
+       
         return true;
-    }
+    }    
+    
     public List<List<String>> partition(String s) {
         List<List<String>> res = new ArrayList<List<String>>();
         List<String> ans = new ArrayList<String>();
-        
-        part(0,s,res,ans);
+        helper(0,s,res,ans);
         return res;
     }
 }
