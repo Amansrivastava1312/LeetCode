737d78dcd54798008ccac52377c1fd4d841302aa
Srivastava Aman S <91677006+Amansrivastava1312@users.noreply.github.com>
2024-01-27 15:23:01 +0530

    Time: 0 ms (100.00%), Space: 44.4 MB (17.59%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
@@ -9,56 +9,55 @@
  * }
  */
 class Solution {
-    ListNode kthNode(ListNode temp , int k ){
-        
+    ListNode Knode(ListNode temp , int k){
         k-=1;
         while(temp!=null && k>0){
-            k--;
             temp=temp.next;
+            k--;
         }
         return temp;
     }
     
-    void reverse(ListNode head){
-        ListNode curr = head;
+  void  reverse(ListNode temp){
+        ListNode curr = temp;
         ListNode prev = null;
         
         while(curr!=null){
             ListNode x = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr=x;
+            curr.next= prev;
+            prev= curr;
+            curr = x;
+            
         }
-        
-        
-    }
+    }    
+    
     public ListNode reverseKGroup(ListNode head, int k) {
-        
-        ListNode temp= head;
+        ListNode temp = head;
         ListNode prevLast = null;
         
         while(temp!=null){
-            ListNode kNode = kthNode(temp , k);
+            ListNode kNode = Knode(temp,k);
+            
             if(kNode == null){
                 if(prevLast!=null){
-                    prevLast.next = temp;
+                    prevLast.next=temp;
                     break;
                 }
             }
-          ListNode  nextNode = kNode.next;
-            kNode.next=null;
             
+            ListNode nextNode = kNode.next;
+            kNode.next = null;
             reverse(temp);
             if(temp==head){
-                head= kNode;
+                head=kNode;
             }else{
-                prevLast.next = kNode;
+                prevLast.next=kNode;
+                // temp=nextNode;
             }
-            prevLast = temp;
-            temp = nextNode;
+            prevLast=temp;
+            temp=nextNode;
             
         }
-        
         return head;
     }
 }
