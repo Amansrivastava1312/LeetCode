051f11bc0944ee3f6c69cc667a3fc4d83733401b
Srivastava Aman S <91677006+Amansrivastava1312@users.noreply.github.com>
2024-01-30 02:08:08 +0530

    Time: 14 ms (70.20%), Space: 45.9 MB (61.79%) - LeetHub

---
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        
+        // HashMap<Integer,Integer> map = new HashMap();
+        Stack <Integer> stack = new Stack();
+        
+       for(int i=nums.length-1;i>=0;i--){
+           stack.push(nums[i]);
+       }
+        
+        
+        for(int i=nums.length-1;i>=0;i--){
+            int num=nums[i];
+            while(!stack.isEmpty() && stack.peek()<=nums[i] ){
+                stack.pop();
+            }
+            
+            if(!stack.isEmpty()){
+                nums[i]=stack.peek();
+            }else{
+                nums[i]=-1;
+            }
+            stack.push(num);
+        }
+        return nums;
+    }
+}
+
+
+
+
+
